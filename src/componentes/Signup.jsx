f23cda523d514179f64f49f8996c488237d6f90f
Carolina Venegas <carolinafran.vs@gmail.com>
2023-02-20 20:13:24 -0300

    subiendo diseños de ejemplos de log in y sign up

--- src/componentes/Signup.jsx
+++ src/componentes/Signup.jsx
@@ -1,8 +1,26 @@
-import "../styles/home.css"
+import "../styles/signup.css"
+import { Input, Button } from '@chakra-ui/react'
+import { ArrowForwardIcon } from '@chakra-ui/icons'
 
-function Signup(){
-    return(
-        <div> <img src="https://assets.justinmind.com/wp-content/uploads/2018/07/mint-sign-up-form.png" alt="" /></div>
+function Signup() {
+    return (
+        <div className="signup">
+            <div className="containerSignup">
+                <h1>Sign up</h1>
+                <div className="data">
+                    <h4>Name:</h4>
+                    <Input width="300px" size='md'></Input>
+                    <h4>Email:</h4>
+                    <Input width="300px" placeholder="user@user.com" size='md'></Input>
+                    <h4>Password:</h4>
+                    <Input width="300px" size='md'></Input>
+                </div>
+                <h5>Si ya tienes cuenta presiona aqui </h5>
+                <Button rightIcon={<ArrowForwardIcon />} colorScheme='teal' variant='outline'>
+                    Log in
+                </Button>
+            </div>
+        </div>
     )
-    }
-    export default Signup
+}
+export default Signup
