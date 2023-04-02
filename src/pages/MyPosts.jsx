59f06945c9558bb71d25ceb8312ff572185b977a
Carolina Venegas <carolinafran.vs@gmail.com>
2023-04-01 23:43:35 -0400

    delete agregado a mis productos

--- src/pages/MyPosts.jsx
+++ src/pages/MyPosts.jsx
@@ -1,84 +1,131 @@
-
-import "../styles/menu.css"
+import "../styles/menu.css";
 import {
-    SimpleGrid, Heading, Button, Card, Image,
-    CardHeader, CardBody, CardFooter, Text
-} from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
-import axios from "axios"
-import { useState, useEffect } from "react"
+  SimpleGrid,
+  Heading,
+  Button,
+  Card,
+  Image,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  Stat,
+  StatLabel,
+  StatHelpText,
+  useToast,
+} from "@chakra-ui/react";
 
-function ProductCardDelete({ img, name, price, descripcion }) {
-    return (
-        <Card>
-            <CardHeader>
-                <Heading size='md'>
-                    <Image
-                        src={img}
-                        alt={name}
-                        borderRadius="lg"
-                    />
-                </Heading>
-            </CardHeader>
-            <CardBody>
-                <Text>
-                    {name}
-                    {descripcion}
-                </Text>
-            </CardBody>
-            <CardFooter className="cardFooter">
-                <Text>{price}</Text>
-                <div className="botones">
-                    <Link to="/product-view"><Button marginRight=" 10px">see more</Button></Link>
-                    <Button marginRight=" 10px">DELETE</Button>
-                </div>
-            </CardFooter>
-        </Card>
-    )
-}
-
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function MyPosts() {
+  const toast = useToast();
 
-    const [misProductos, setMisProductos] = useState([]);
-    useEffect(() => {
-        const getMisProductos = async () => {
-            try {
-                const token = localStorage.getItem("token");
+  const [misProductos, setMisProductos] = useState([]);
+  useEffect(() => {
+    const obtenerMisProductos = async () => {
+      try {
+        const token = localStorage.getItem("token");
 
-                const headers = {
-                    'content-type': 'application/json',
-                    'authorization': 'Bearer ' + token
-                };
-
-                const { data } = await axios.get("http://localhost:4000/producto", { headers });
-                setMisProductos(data);
-            }
-            catch (error) {
-                console.log(error);
-            }
+        const headers = {
+          "content-type": "application/json",
+          authorization: "Bearer " + token,
         };
-        getMisProductos();
-    }, []);
 
-    return (
+        const { data } = await axios.get("http://localhost:4000/producto", {
+          headers,
+        });
+        setMisProductos(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    obtenerMisProductos();
+  }, []);
 
-        <div className="myposts">
-            <div className="box" style={{ "height": "100vh" }}>
-                <div className="contenidoPosts">
+  async function eliminarProducto(id) {
+    try {
+      const token = localStorage.getItem("token");
 
-                    <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
-                        {misProductos.map((producto) =>
-                        (<ProductCardDelete
-                            img={producto.img}
-                            name={producto.nombre}
-                            descripcion={producto.descripcion}
-                            price={producto.price}
-                        />))}
-                    </SimpleGrid>
-                </div>
-            </div>
+      const headers = {
+        "content-type": "application/json",
+        authorization: "Bearer " + token,
+      };
+
+      const {
+        data: { producto },
+      } = await axios.delete(`http://localhost:4000/producto/${id}`, {
+        headers,
+      });
+
+      const nuevoMisProductos = misProductos.filter(
+        (miProducto) => miProducto.id !== producto.id
+      );
+
+      setMisProductos(nuevoMisProductos);
+      toast({
+        title: "Producto eliminado",
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+      });
+    } catch (error) {
+      toast({
+        title: "Hubo un error al eliminar el producto. Intentelo de nuevo.",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  }
+
+  return (
+    <div className="myposts">
+      <div className="box" style={{ height: "100vh" }}>
+        <div className="contenidoPosts">
+          <SimpleGrid
+            spacing={4}
+            templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+          >
+            {misProductos.map((producto) => (
+              <Card>
+                <CardHeader>
+                  <Heading size="md">
+                    <Image
+                      src={producto.img}
+                      alt={producto.titulo}
+                      borderRadius="lg"
+                    />
+                  </Heading>
+                </CardHeader>
+                <CardBody>
+                  <Stat>
+                    <StatLabel>{producto.titulo}</StatLabel>
+                    <StatHelpText>{producto.descripcion}</StatHelpText>
+                  </Stat>
+                </CardBody>
+                <CardFooter className="cardFooter">
+                  <Text>{producto.price}</Text>
+                  <div className="botones">
+                    <Link to="/product-view">
+                      <Button marginRight=" 10px">Ver más</Button>
+                    </Link>
+                    <Button
+                      onClick={() => eliminarProducto(producto.id)}
+                      marginRight=" 10px"
+                      colorScheme="red"
+                    >
+                      Eliminar
+                    </Button>
+                  </div>
+                </CardFooter>
+              </Card>
+            ))}
+          </SimpleGrid>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-export default MyPosts
+export default MyPosts;
